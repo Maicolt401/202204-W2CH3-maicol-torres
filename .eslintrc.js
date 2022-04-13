e8d288e3b540aaae590bf92d04f71cc1e6f29f20
maicol torres <maicolt401@gmail.com>
2022-04-13 18:25:43 +0200

    herramienteishon installed

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    eslint: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
